9bfa1eef92aeca91382f1118a4e912a73ea2a4c2
sayuj1 <sayuj.sehgal@gmail.com>
2023-06-16 10:01:53 +0530

    Added Bill Management

--- repository/transactionRepository.js
+++ repository/transactionRepository.js
@@ -24,7 +24,7 @@ exports.addTransactionDetails = async (
   user
 ) => {
   let result = await connectionPromise(
-    `INSERT INTO transactions (user_id, type, amount, date, description, category, remarks, category_others) VALUES ('${user.user_id}', '${transaction.type}', '${transaction.amount}', '${transaction.date}', '${transaction.description}', '${transaction.category}', '${transaction.remarks}', '${transaction.category_others}')`
+    `INSERT INTO transactions (user_id, type, amount, date, description, category, remarks, category_others, bill_id) VALUES ('${user.user_id}', '${transaction.type}', '${transaction.amount}', '${transaction.date}', '${transaction.description}', '${transaction.category}', '${transaction.remarks}', '${transaction.category_others}', '${transaction?.bill_id}')`
   );
   return result;
 };
